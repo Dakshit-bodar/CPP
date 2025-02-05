a57a1a2e07791c7e7af47815f8f0f32a24c7ae3c
Dakshit-bodar <bodardakshit2@gmail.com>
2025-02-05 17:49:57 +0530

    05/02/2025

--- calculator mark.cpp
+++ calculator mark.cpp
@@ -2,25 +2,42 @@
 using namespace std;
 
 main(){
-	int marks;
-	cout<<"Enter Your Marks :-";
-	cin>>marks;
-	
-	if(marks >= 90){
-		cout<<"Your Gread A+";
-	}else if(marks >= 80){
-		cout<<"Your Gread A";
-	}else if(marks >= 70){
-		cout<<"Your Gread B+";
-	}else if(marks >= 60){
-		cout<<"Your Gread B+";
-	}else if(marks >= 50){
-		cout<<"Your Gread C";
-	}else if(marks >= 40){
-		cout<<"Your Gread D";
-	}else if(marks >= 30){
-		cout<<"Your Are Faile";
+    int marks;
+
+    cout<<"Enter your marks :- ";
+    cin>>marks;
+    
+    if(marks>=0 && marks<=100){
+    	if(marks>=90){
+    		cout <<"Grade: A+" << endl;
+            cout <<"Congratulations! You are eligible for the next level."<<endl;
+		}
+		else if(marks>=80){
+			cout <<"Grade: A" << endl;
+            cout <<"Congratulations! You are eligible for the next level."<<endl;
+		}
+		else if(marks>=70){
+		    cout << "Grade: B+" << endl;
+            cout << "Congratulations! You are eligible for the next level."<<endl;
+		}
+		else if(marks>=60){
+			cout << "Grade: B" << endl;
+            cout << "Congratulations! You are eligible for the next level."<<endl;
+		}
+		else if(marks>=50){
+			cout << "Grade: C" << endl;
+            cout << "Congratulations! You are eligible for the next level."<<endl;
+		}
+		else if(marks>=40){
+			cout << "Grade: D" << endl;
+            cout << "Congratulations! You are eligible for the next level."<<endl;
+		}else if(marks>33){
+			cout << "Grade: F"<<endl;
+            cout << "Please try again next time."<<endl;
+		}
 	}else{
-		cout<<"An Valide Marks";
+		cout<<"Invalid marks Enter 0 and 100."<<endl;
 	}
 }
+    
+  
